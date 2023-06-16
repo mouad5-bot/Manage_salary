2ecdb54135385f1cc91fdd658aa438f085938609
MOUAD FIFEL <mouad.fifel5@gmail.com>
2023-06-16 11:59:23 +0100

    done with monthSalary of gestionnaire

--- application/classes/Gestionnaire.php
+++ application/classes/Gestionnaire.php
@@ -1,8 +1,19 @@
 <?php 
+include 'Employe.php';
 	class Gestionnaire extends Employe {
 		public $id;
+		public $salaire_de_base = 3000;
+	
 		public function salaire($bonus_annuel){
+			$this->salaire_mensuel = $this->salaire_de_base + ($bonus_annuel / 12);
+		}
 
+		public function getSalaire(){
+			return $this->salaire_mensuel;
 		}
 	}
+
+	$obj = new Gestionnaire();
+	$obj->salaire(1200);
+	var_dump($obj);
 ?>
